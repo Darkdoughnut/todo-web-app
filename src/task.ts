17bdde6780bed08ea4ed2fb42d580b631b1d5e97
Caleb Jones <code@calebjones.net>
2017-07-06 01:11:47 -0400

    Add a higher level interface to the event log
    The event log still operates by consuming events, but you no longer create the
    events by hand, instead you call methods on tasks to generate events that act on
    them. For instance, a task is responsible for generating the message that would
    add a tag to it.

--- src/task.ts
+++ src/task.ts
@@ -8,28 +8,88 @@ export class Task {
     finishedTimestamp?: Date;
     tags: Tag[];
     dependencies: UniqueId[];
+
+    constructor({ id, title, createdTimestamp, finishedTimestamp, tags, dependencies }:
+        { id: UniqueId, title: string, createdTimestamp: Date, finishedTimestamp?: Date, tags: Tag[], dependencies: UniqueId[] }) {
+        this.id = id;
+        this.title = title;
+        this.createdTimestamp = createdTimestamp;
+        this.finishedTimestamp = finishedTimestamp;
+        this.tags = tags;
+        this.dependencies = dependencies;
+    }
+
+    addTagEvt(tag: Tag): AddTagTaskEvent {
+        return {
+            type: TaskEventType.addTag,
+            timestamp: new Date(),
+            id: this.id,
+            tag: tag,
+        };
+    }
+
+    addDepEvt(dep: Task | UniqueId): AddDepTaskEvent {
+        let depId = dep instanceof Task ? dep.id : dep;
+        return {
+            type: TaskEventType.addDep,
+            timestamp: new Date(),
+            id: this.id,
+            dep: depId,
+        };
+    }
+
+    finishEvt(): FinishTaskEvent {
+        return {
+            type: TaskEventType.finish,
+            timestamp: new Date(),
+            id: this.id,
+        };
+    }
+
 }
 
-export enum TaskEventType {
+enum TaskEventType {
     create = 'create',
     addTag = 'addTag',
     addDep = 'addDependency',
     finish = 'finish',
 }
 
-export type TaskEvent
-    = { type: TaskEventType.create, timestamp: Date, id: UniqueId, title: string }
-    | { type: TaskEventType.addTag, timestamp: Date, id: UniqueId, tag: Tag }
-    | { type: TaskEventType.addDep, timestamp: Date, id: UniqueId, dep: UniqueId }
-    | { type: TaskEventType.finish, timestamp: Date, id: UniqueId }
+type CreateTaskEvent = { type: TaskEventType.create, timestamp: Date, id: UniqueId, title: string }
+type AddTagTaskEvent = { type: TaskEventType.addTag, timestamp: Date, id: UniqueId, tag: Tag }
+type AddDepTaskEvent = { type: TaskEventType.addDep, timestamp: Date, id: UniqueId, dep: UniqueId }
+type FinishTaskEvent = { type: TaskEventType.finish, timestamp: Date, id: UniqueId }
+
+type TaskEvent
+    = CreateTaskEvent
+    | AddTagTaskEvent
+    | AddDepTaskEvent
+    | FinishTaskEvent
 
 export class TaskLog {
     private events: TaskEvent[];
     private tasks: { [id: number]: Task };
+    private liveTasks: UniqueId[];
+    private idCounter: UniqueId;
 
     constructor() {
         this.events = [];
         this.tasks = {};
+        this.liveTasks = [];
+        this.idCounter = 0;
+    }
+
+    createTask(title: string, timestamp?: Date): Task {
+        // @Todo: This should be a unique id derived from a counter, the user, and the device
+        if (!timestamp) {
+            timestamp = new Date();
+        }
+        let id = ++this.idCounter;
+        this.event({
+            type: TaskEventType.create,
+            timestamp, id, title,
+        });
+        return this.getTaskForId(id);
     }
 
     event(evt: TaskEvent) {
@@ -38,40 +98,47 @@ export class TaskLog {
             evt = { ...evt, timestamp: new Date() };
         }
 
+        let task = this.tasks[evt.id];
         switch (evt.type) {
             case TaskEventType.create: {
-                this.tasks[evt.id] = {
+                if (task) { break; }
+                this.tasks[evt.id] = new Task({
                     id: evt.id,
                     title: evt.title,
                     createdTimestamp: evt.timestamp,
                     tags: [],
                     dependencies: [],
-                };
+                });
+                this.liveTasks.push(evt.id);
                 break;
             }
             case TaskEventType.addTag: {
-                let task = this.tasks[evt.id];
                 if (!task) { break; }
-                this.tasks[evt.id] = { ...task, tags: listSetAdd(task.tags, evt.tag) };
+                this.tasks[evt.id] = new Task({ ...task, tags: listSetAdd(task.tags, evt.tag) });
                 break;
             }
             case TaskEventType.addDep: {
-                let task = this.tasks[evt.id];
                 if (!task) { break; }
-                this.tasks[evt.id] = { ...task, dependencies: listSetAdd(task.dependencies, evt.dep) };
+                this.tasks[evt.id] = new Task({ ...task, dependencies: listSetAdd(task.dependencies, evt.dep) });
                 break;
             }
             case TaskEventType.finish: {
-                let task = this.tasks[evt.id];
                 if (!task) { break; }
-                delete this.tasks[evt.id];
+                let idx = this.liveTasks.indexOf(evt.id);
+                if (idx !== -1) {
+                    this.liveTasks.splice(idx, 1);
+                }
                 break;
             }
         }
     }
 
     getTasks(): Task[] {
-        return Object.keys(this.tasks).map(k => this.tasks[k]);
+        return this.liveTasks.map(id => this.tasks[id]);
+    }
+
+    getTaskForId(id: UniqueId): Task | undefined {
+        return this.tasks[id];
     }
 }
 
--- test/Task.test.ts
+++ test/Task.test.ts
@@ -1,16 +1,31 @@
 import { expect } from 'chai';
-import { Task, TaskLog, TaskEventType, TaskEvent } from '../src/Task';
+import { Task, TaskLog } from '../src/Task';
 
 describe("Task", () => {
-    it("should create a task", () => {
-        let task: Task = {
+    it("Should create a task with the constructor", () => {
+        let task = new Task({
             id: 0,
             title: "Do homework",
             createdTimestamp: new Date(),
             tags: ["HW", "281"],
             dependencies: [],
-        };
+        });
         expect(task.finishedTimestamp).undefined;
+    });
+
+    it("Should create task events", () => {
+        let task = new Task({
+            id: 13,
+            title: "Create a task",
+            createdTimestamp: new Date(),
+            tags: [],
+            dependencies: [],
+        });
+        expect(task.addDepEvt(1).id).eql(task.id);
+        expect(task.addDepEvt(42).dep).eql(42);
+        expect(task.addTagEvt('Code:Todo-App').id).eql(task.id)
+        expect(task.addTagEvt('Code:Todo-App').tag).eql('Code:Todo-App');
+        expect(task.finishEvt().id).eql(task.id);
     });
 });
 
@@ -18,128 +33,69 @@ describe("TaskLog", () => {
     it("Should create tasks in the tasklog", () => {
         let taskLog = new TaskLog();
         let startDate = new Date();
-        taskLog.event({
-            type: TaskEventType.create,
-            timestamp: startDate,
-            id: 0,
-            title: 'Hello, World',
-        });
-        expect(taskLog.getTasks()).eql([{
-            id: 0,
+        let task1 = taskLog.createTask("Hello, World", startDate);
+        expect(taskLog.getTasks()).eql([task1]);
+        expect(taskLog.getTasks()).eql([new Task({
+            id: task1.id,
             title: 'Hello, World',
             createdTimestamp: startDate,
             tags: [],
             dependencies: [],
-        }]);
+        })]);
         let date2 = new Date();
-        taskLog.event({
-            type: TaskEventType.create,
-            timestamp: date2,
-            id: 1,
-            title: 'coi lo plini',
-        });
-        expect(taskLog.getTasks()).eql([{
-            id: 0,
+        let task2 = taskLog.createTask("coi lo plini");
+        expect(taskLog.getTasks()).eql([task1, task2]);
+        expect(taskLog.getTasks()).eql([new Task({
+            id: task1.id,
             title: 'Hello, World',
             createdTimestamp: startDate,
             tags: [],
             dependencies: [],
-        },
-        {
-            id: 1,
+        }),
+        new Task({
+            id: task2.id,
             title: 'coi lo plini',
             createdTimestamp: date2,
             tags: [],
             dependencies: [],
-        }]);
+        })]);
     });
 
     it("Should add tags to tasks", () => {
         let taskLog = new TaskLog();
-        taskLog.event({
-            type: TaskEventType.create,
-            timestamp: new Date(),
-            id: 0,
-            title: 'Make bread',
-        });
+        let task = taskLog.createTask('Make bread');
         expect(taskLog.getTasks().map(x => x.tags)).eql([[]]);
-        taskLog.event({
-            type: TaskEventType.addTag,
-            timestamp: new Date(),
-            id: 0,
-            tag: 'Cooking',
-        });
+        taskLog.event(task.addTagEvt('Cooking'));
         expect(taskLog.getTasks().map(x => x.tags)).eql([['Cooking']]);
-        taskLog.event({
-            type: TaskEventType.addTag,
-            timestamp: new Date(),
-            id: 0,
-            tag: 'Bread',
-        });
+        taskLog.event(task.addTagEvt('Bread'));
         expect(taskLog.getTasks().map(x => x.tags)).eql([['Cooking', 'Bread']]);
     });
 
     it("Shouldn't add duplicate tags", () => {
         let taskLog = new TaskLog();
-        taskLog.event({
-            type: TaskEventType.create,
-            timestamp: new Date(),
-            id: 0,
-            title: 'Integration junk',
-        });
+        let task = taskLog.createTask("Integration junk");
         expect(taskLog.getTasks().map(x => x.tags)).eql([[]]);
-        let homeworkTag: TaskEvent = {
-            type: TaskEventType.addTag,
-            timestamp: new Date(),
-            id: 0,
-            tag: 'Homework',
-        };
-        taskLog.event({ ...homeworkTag });
+        taskLog.event(task.addTagEvt('Homework'));
         expect(taskLog.getTasks().map(x => x.tags)).eql([['Homework']]);
-        taskLog.event({ ...homeworkTag });
+        taskLog.event(task.addTagEvt('Homework'));
         expect(taskLog.getTasks().map(x => x.tags)).eql([['Homework']]);
     });
 
     it("Should add dependencies", () => {
         let taskLog = new TaskLog();
-        taskLog.event({
-            type: TaskEventType.create,
-            timestamp: new Date(),
-            id: 0,
-            title: 'Task A',
-        });
-        taskLog.event({
-            type: TaskEventType.create,
-            timestamp: new Date(),
-            id: 1,
-            title: 'Task B',
-        });
-        taskLog.event({
-            type: TaskEventType.addDep,
-            timestamp: new Date(),
-            id: 0,
-            dep: 1,
-        });
+        let task1 = taskLog.createTask("Task A");
+        let task2 = taskLog.createTask("Task B");
+        taskLog.event(task1.addDepEvt(task2));
         let tasks = taskLog.getTasks();
-        expect(tasks.map(x => x.title)).eql(['Task A', 'Task B']);
-        expect(tasks.map(x => x.id)).eql([0, 1]);
-        expect(tasks.map(x => x.dependencies)).eql([[1], []]);
+        expect(tasks.map(x => x.title)).eql(["Task A", "Task B"]);
+        expect(tasks.map(x => x.dependencies)).eql([[task2.id], []]);
     });
 
     it("Should remove tasks", () => {
         let taskLog = new TaskLog();
-        taskLog.event({
-            type: TaskEventType.create,
-            timestamp: new Date(),
-            id: 0,
-            title: 'A pointless task',
-        });
+        let task = taskLog.createTask("A pointless task");
         expect(taskLog.getTasks().length).eql(1);
-        taskLog.event({
-            type: TaskEventType.finish,
-            timestamp: new Date(),
-            id: 0,
-        });
+        taskLog.event(task.finishEvt());
         expect(taskLog.getTasks().length).eql(0);
     });
 });
